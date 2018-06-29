1ebbc6bd32dc6fd93c94d09c99ae1504d7c4a314
akashkahlon <akashkahlon11@gmail.com>
2018-06-29 08:55:54 +0530

    remove unwanted test data from lxd_profile spec

--- spec/clients/lxd_profile_spec.rb
+++ spec/clients/lxd_profile_spec.rb
@@ -1,69 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe LxdProfile do
-  get_default_profile = {:config=>
-                         {:"user.network-config"=>
-                          "version: 1\n" +
-                            "config:\n" +
-                            "  - type: physical\n" +
-                            "    name: eth0\n" +
-                            "    subnets:\n" +
-                            "      - type: dhcp\n" +
-                            "        control: auto\n" +
-                            "  - type: nameserver\n" +
-                            "    address: 172.16.200.200\n",
-                            :"user.user-data"=>
-                          "#cloud-config\n" +
-                            "package_upgrade: true\n" +
-                            "ssh_authorized_keys:\n" +
-                            " - \n" +
-                            "runcmd:\n" +
-                            "  - touch /tmp/nsupdate\n" +
-                            "  - echo \"server 172.16.200.200\" >> /tmp/nsupdate\n" +
-                            "  - echo \"zone lxd\" >> /tmp/nsupdate\n" +
-                            "  - echo \"update delete $(hostname).lxd A\" >> /tmp/nsupdate\n" +
-                            "  - echo \"update add $(hostname).lxd 60 A $(hostname -I | awk '{print $1}')\" >> /tmp/nsupdate\n" +
-                            "  - echo \"send\" >> /tmp/nsupdate\n" +
-                            "  - nsupdate -v /tmp/nsupdate\n"},
-                            :description=>"Default LXD profile",
-                            :devices=>
-                          {:eth0=>{:name=>"eth0", :nictype=>"bridged", :parent=>"fan10", :type=>"nic"},
-                           :root=>{:path=>"/", :pool=>"local", :type=>"disk"}},
-                          :name=>"default",
-                          :used_by=>["/1.0/containers/test-1"]}
-  get_new_profile = {:config=>
-                     {:"limits.cpu"=>"1",
-                      :"limits.memory"=>"100MB",
-                      :"user.network-config"=>
-                     "version: 1\n" +
-                       "config:\n" +
-                       "  - type: physical\n" +
-                       "    name: eth0\n" +
-                       "    subnets:\n" +
-                       "      - type: dhcp\n" +
-                       "        control: auto\n" +
-                       "  - type: nameserver\n" +
-                       "    address: 172.16.200.200\n",
-                       :"user.user-data"=>
-                     "#cloud-config\n" +
-                       "package_upgrade: true\n" +
-                       "ssh_authorized_keys:\n" +
-                       " - \n" +
-                       "runcmd:\n" +
-                       "  - touch /tmp/nsupdate\n" +
-                       "  - echo \"server 172.16.200.200\" >> /tmp/nsupdate\n" +
-                       "  - echo \"zone lxd\" >> /tmp/nsupdate\n" +
-                       "  - echo \"update delete $(hostname).lxd A\" >> /tmp/nsupdate\n" +
-                       "  - echo \"update add $(hostname).lxd 60 A $(hostname -I | awk '{print $1}')\" >> /tmp/nsupdate\n" +
-                       "  - echo \"send\" >> /tmp/nsupdate\n" +
-                       "  - nsupdate -v /tmp/nsupdate\n"},
-                       :description=>"Default LXD profile",
-                       :devices=>
-                     {:eth0=>{:name=>"eth0", :nictype=>"bridged", :parent=>"fan10", :type=>"nic"},
-                      :root=>{:path=>"/", :pool=>"local", :type=>"disk"}},
-                     :name=>"default",
-                     :used_by=>["/1.0/containers/test-1"]}
-
   before(:each) do
     ContainerHost.create(hostname: 'p-ubuntu-01', ipaddress: '172.16.33.33')
   end
