3691dd752d73a3921138f8d0b50b8de3f467d7cb
akashkahlon <akashkahlon11@gmail.com>
2018-07-02 17:04:26 +0530

    add create method in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,16 @@
 class ProfilesController < ApplicationController
+  def create
+    profile = Profile.new(name: params[:name], ssh_authorized_keys: params[:ssh_authorized_keys],
+                          cpu_limit: params[:cpu_limit], memory_limit: params[:memory_limit])
+    unless profile.valid?
+      render json: {success: false, errors: profile.errors.full_messages.join(',')}, status: :bad_request
+      return
+    end
+
+    unless profile.create
+      render json: {success: false, errors: profile.errors.full_messages.join(',')}, status: :server_error
+      return
+    end
+    render json: {success: true, errors: ''}, status: :created
+  end
 end
---
+++ spec/controllers/profiles_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe ProfilesController, type: :controller do
+  login_user
+
+  before(:each) do
+    ContainerHost.create(hostname: 'p-ubuntu-01', ipaddress: '172.16.33.33')
+  end
+
+  describe 'POST#create' do
+    context 'success', :vcr do
+      it 'creates a new profile', :delete_profile_after, profile_name: 'medium' do
+        post :create, params: {name: 'medium', cpu_limit: '1', memory_limit: '100MB', ssh_authorized_keys: ['abc','def']}
+
+        expect(JSON.parse(response.body)['success']).to eq(true)
+        expect(JSON.parse(response.body)['errors']).to eq('')
+        expect(response.code).to eq('201')
+      end
+    end
+
+    context 'failure' do
+      it 'should return bad request for invalid profile' do
+        post :create, params: {name: '', cpu_limit: '1', memory_limit: '100MB', ssh_authorized_keys: ['abc','def']}
+
+        expect(JSON.parse(response.body)['success']).to eq(false)
+        expect(JSON.parse(response.body)['errors']).to eq("Name can't be blank")
+        expect(response.code).to eq('400')
+      end
+
+      it 'should return server error for failed create operation' do
+        allow_any_instance_of(Profile).to receive(:create).and_return(false)
+        post :create, params: {name: 'bad-value'}
+
+        expect(JSON.parse(response.body)['success']).to eq(false)
+        expect(response.code).to eq('500')
+      end
+    end
+  end
+end
