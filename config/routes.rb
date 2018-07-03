62cdf15859209d1f645911ba436e9cac83e72e06
akashkahlon <akashkahlon11@gmail.com>
2018-07-03 12:32:25 +0530

    add link to profiles list on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,6 @@ Rails.application.routes.draw do
 
   resources :key_pairs, except: [:edit, :update]
 
-  post '/profiles', to: 'profiles#create'
-  get '/profiles', to: 'profiles#index'
+  post '/profiles', to: 'profiles#create', as: 'profiles_create'
+  get '/profiles', to: 'profiles#index', as: 'profiles'
 end
