ed3e7a36cc2fd197a8e1c9473cb220923b42fc85
Muhammad Fadli Nurhasan <fadli.nurhasan@go-jek.com>
2018-07-18 16:14:54 +0700

    [fadli/tara] add continuous poll until container surely started

--- app/jobs/start_container.rb
+++ app/jobs/start_container.rb
@@ -9,9 +9,16 @@ class StartContainer
   def perform(container_name)
     container = client.container(container_name)
     start_interval = Time.now - container[:created_at]
+    container_status = container[:status]
+    count = 0
     if start_interval > Figaro.env.WAIT_INTERVAL_FOR_CONTAINER_OPERATIONS.to_i
-      if container[:status] != "Running"
-        client.start_container(container_name)
+      while container_status != "Running" || count == Figaro.env.MAX_RETRY.to_i
+        sleep(Figaro.env.WAIT_INTERVAL_FOR_CONTAINER_OPERATIONS.to_i)
+        res = client.start_container(container_name)
+        container_detail = client.container_state(container_name)
+        container_status = container_detail[:status]
+        count += 1
+        res
       end
     else
       raise Exception.new("Container #{container_name} is still being created")
